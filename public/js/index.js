60dde34c6b32f6810f35af318851a77de997b867
brizism <montielbriseida@gmail.com>
2018-06-25 21:12:10 -0400

    grid layouts

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+const [current, imgs] = [document.querySelector('#current'), document.querySelectorAll('.imgs img')];
+const opacity = 0.6;
+
+// Set first img opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+  // Reset the opacity
+  imgs.forEach(img => (img.style.opacity = 1));
+
+  // Change current img to scr of clicked img
+  current.src = e.target.src;
+
+  // Add fade in class
+  current.classList.add('fade-in');
+
+  // Remove fade-in class after .5 seconds
+  setTimeout(() => current.classList.remove('fade-in'), 500);
+
+  // Change the opacity to opacity var
+  e.target.style.opacity = opacity;
+}
